7a14198f7aba21140fbad49212a8d3a7bba63dbd
Philip <wowcxy2008@126.com>
2021-02-22 23:34:09 +0800

    sth

---
+++ server.js
@@ -0,0 +1,15 @@
+/**
+ * 服务端
+ * @author Philip
+ */
+const Koa = require('koa')
+const app = new Koa()
+const path = require('path')
+
+app.use(require('koa-static')(path.join(__dirname) + '/html'));
+
+app.use(async (ctx) => {
+  ctx.body = 'web practice';
+});
+
+app.listen(3333);
