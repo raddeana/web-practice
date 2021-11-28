2d9e3d4f45c14adb6ac93b8767da9ce4324d8f2d
Philip <wowcxy2008@126.com>
2021-11-28 19:32:14 +0800

    feat: sth

---
+++ babel/main.babel.js
@@ -0,0 +1,26 @@
+import _classCallCheck from "@babel/runtime-corejs3/helpers/classCallCheck";
+
+var _context;
+
+import _Map from "@babel/runtime-corejs3/core-js-stable/map";
+import _Promise from "@babel/runtime-corejs3/core-js-stable/promise";
+import _includesInstanceProperty from "@babel/runtime-corejs3/core-js-stable/instance/includes";
+
+/**
+ * main
+ * @author Philip
+ */
+var fn = function fn() {};
+
+new _Map();
+new _Promise(function () {});
+
+var Test = function Test() {
+  _classCallCheck(this, Test);
+};
+
+;
+
+var c = _includesInstanceProperty(_context = [1, 2, 3]).call(_context, 1);
+
+var aaa = 10;
