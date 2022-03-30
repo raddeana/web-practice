9e2399be527fea9a3a198c0a7a0dedf1160a6a05
Chen Xiangyu <wowcxy2008@126.com>
2022-03-30 16:26:42 +0800

    sth

--- checks/vue-plugins/src/web-components.js
+++
@@ -1,9 +0,0 @@
-/**
- * web组件注册
- * @author Philip
- */
-import Vue from 'vue';
-import wrap from '@vue/web-component-wrapper';
-import item from './components/item.vue';
-
-customElements.define('wc-item', wrap(Vue, item));
